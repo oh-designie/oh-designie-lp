06285d91cb36f7fd5351e5b083409f04bb5facd8
Yuria <nishimura.yuria@gmail.com>
2018-05-30 12:44:51 -0700

    feat(header): optimize header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import { Button, Col, Layout, Row } from 'antd';
 
 import { LocaleType } from '../locales';
+import { colorMap, spaceMap, Styles } from '../styles';
 
 interface HeaderProps {
   readonly textMap: LocaleType;
 }
 
 export const Header = ({ textMap }: HeaderProps) => (
-  <Layout.Header className="header">
+  <Layout.Header style={styles.header}>
     <Row type="flex" justify="space-between" align="middle">
       <Col>
         <a className="logo">{textMap.appTitle}</a>
@@ -19,3 +20,11 @@ export const Header = ({ textMap }: HeaderProps) => (
     </Row>
   </Layout.Header>
 );
+
+type StyleKey = 'header';
+const styles: Styles<StyleKey> = {
+  header: {
+    backgroundColor: colorMap.primary,
+    boxShadow: `0 ${spaceMap.xs} ${spaceMap.sm} ${colorMap.shadow}`,
+  },
+};
