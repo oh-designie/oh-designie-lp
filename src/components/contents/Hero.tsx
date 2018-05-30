0287469c96257e0ba4155e354e4c0a0f0265716c
Yuria <nishimura.yuria@gmail.com>
2018-05-30 13:47:04 -0700

    feat(hero): optimize hero area

--- src/components/contents/Hero.tsx
+++ src/components/contents/Hero.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import { Col, Row } from 'antd';
 
+import { colorMap, heightMap, spaceMap, Styles } from '../../styles';
+
+import pattern from '../../assets/images/bg_pattern.png';
 import smartphone from '../../assets/images/hero_smartphone.svg';
 
 export const Hero = () => (
-  <Row className="pat-container" type="flex" justify="center" align="top">
-    <Col>
-      <img className="img-phone" src={smartphone} />
+  <Row style={styles.container} type="flex" justify="center" align="top">
+    <Col md={16} lg={12}>
+      <img style={styles.img} src={smartphone} />
     </Col>
   </Row>
 );
+
+type StyleKey = 'container' | 'img';
+const styles: Styles<StyleKey> = {
+  container: {
+    backgroundColor: colorMap.white,
+    backgroundImage: `url(${pattern})`,
+    backgroundRepeat: 'repeat',
+    height: heightMap.hero,
+  },
+  img: {
+    marginTop: spaceMap.sm,
+    maxWidth: '100%',
+  },
+};
