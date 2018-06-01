cd68ed0719c268bb79ef8243e6977b6743ffccf2
Yuria <nishimura.yuria@gmail.com>
2018-05-31 21:15:24 -0700

    feat(layout): clip hero and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,7 @@ import {
   colorMap,
   fontSizeMap,
   fontFamilyMap,
+  lengthMap,
   spaceMap,
   Styles,
 } from '../styles';
@@ -19,50 +20,66 @@ interface FooterProps {
 }
 
 export const Footer = ({ textMap }: FooterProps) => (
-  <Layout.Footer style={styles.footer}>
-    <Row
-      style={styles.content}
-      gutter={16}
-      type="flex"
-      justify="center"
-      align="middle"
-    >
-      <Col>
-        <img height="40" alt={textMap.appStore} src={appStoreBadge} />
-      </Col>
-      <Col>
-        <img height="40" alt={textMap.googlePlay} src={googlePlayBadge} />
-      </Col>
-    </Row>
-    <Row style={styles.content} type="flex" justify="center" align="middle">
-      <Col>
-        <IconButton href="" type="github" />
-        <IconButton href="" type="instagram" />
-        <IconButton href="" type="twitter" />
-      </Col>
-    </Row>
-    <Row type="flex" justify="space-between" align="middle">
-      <Col>
-        <small>{textMap.copyright}</small>
-      </Col>
-      <Col>
-        <Link href="/" text={textMap.termsOfUse} />
-        <span> | </span>
-        <Link href="/" text={textMap.privacyPolicy} />
-      </Col>
-    </Row>
-  </Layout.Footer>
+  <div style={styles.footer}>
+    <Layout.Footer style={styles.container}>
+      <Row
+        style={styles.content}
+        gutter={16}
+        type="flex"
+        justify="center"
+        align="middle"
+      >
+        <Col>
+          <img height="40" alt={textMap.appStore} src={appStoreBadge} />
+        </Col>
+        <Col>
+          <img height="40" alt={textMap.googlePlay} src={googlePlayBadge} />
+        </Col>
+      </Row>
+      <Row style={styles.content} type="flex" justify="center" align="middle">
+        <Col>
+          <IconButton href="" type="github" />
+          <IconButton href="" type="instagram" />
+          <IconButton href="" type="twitter" />
+        </Col>
+      </Row>
+      <Row
+        style={styles.links}
+        type="flex"
+        justify="space-between"
+        align="middle"
+      >
+        <Col>
+          <small>{textMap.copyright}</small>
+        </Col>
+        <Col>
+          <Link href="/" text={textMap.termsOfUse} />
+          <span> | </span>
+          <Link href="/" text={textMap.privacyPolicy} />
+        </Col>
+      </Row>
+    </Layout.Footer>
+  </div>
 );
 
-type StyleKey = 'footer' | 'content';
+type StyleKey = 'container' | 'content' | 'footer' | 'links';
 const styles: Styles<StyleKey> = {
-  footer: {
-    fontFamily: fontFamilyMap.body,
+  container: {
     backgroundColor: colorMap.primary,
     color: colorMap.translucent,
+    fontFamily: fontFamilyMap.body,
     fontSize: fontSizeMap.sm,
   },
   content: {
     marginTop: spaceMap.xxl,
+  },
+  footer: {
+    clipPath: `circle(${lengthMap.footerCircle} at 50% 760%)`,
+  },
+  links: {
+    marginTop: spaceMap.xs,
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    maxWidth: lengthMap.chapter,
   },
 };
