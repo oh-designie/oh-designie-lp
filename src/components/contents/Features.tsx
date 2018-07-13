89feabc43878524cd50b3862b0c4e5c01b722346
Yuria <nishimura.yuria@gmail.com>
2018-07-13 00:34:38 -0700

    feat(games): create games section

--- src/components/contents/Features.tsx
+++ src/components/contents/Features.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Col, List, Row } from 'antd';
+import { Col, Row } from 'antd';
 import { pure } from 'recompose';
 
 import { Chapter, Heading } from './../';
@@ -18,15 +18,20 @@ import practiceIcon from '../../assets/images/icons/practice.svg';
 import colorIcon from '../../assets/images/categories/color.svg';
 import fontIcon from '../../assets/images/categories/font.svg';
 import layoutIcon from '../../assets/images/categories/layout.svg';
+import brightnessIcon from '../../assets/images/games/brightness.png';
+import colorCode1Icon from '../../assets/images/games/color_code1.png';
+import majorityCaseIcon from '../../assets/images/games/majority_case.png';
+import majorityShapeIcon from '../../assets/images/games/majority_shape.png';
+import typefaceIcon from '../../assets/images/games/typeface.png';
+import rectangleIcon from '../../assets/images/games/rectangle.png';
 
 interface FeaturesProps {
   readonly textMap: LocaleType;
 }
 
 export const Features = pure(({ textMap }: FeaturesProps) => {
-  const { title, h1, section1, section2 } = textMap.contents.features;
-  const { color, font, layout } = textMap.categories;
-  const { competition, practice } = textMap.mode;
+  const { categories, modes, games } = textMap;
+  const { title, h1, section1, section2, section3 } = textMap.contents.features;
   return (
     <Chapter styles={styles}>
       <Heading size="lg" text={title} />
@@ -36,56 +41,34 @@ export const Features = pure(({ textMap }: FeaturesProps) => {
         <p>{section1.desc}</p>
         <Row
           style={styles.imgContainer}
-          gutter={24}
+          gutter={8}
           type="flex"
-          justify="space-around"
+          justify="center"
           align="middle"
         >
-          <Col span={12}>
-            <List
-              size="large"
-              bordered
-              header={
-                <figure style={styles.icon}>
-                  <img
-                    style={styles.icon}
-                    height={fontSizeMap.lg}
-                    alt={competition}
-                    src={competitionIcon}
-                  />
-                  <figcaption style={styles.caption}>{competition}</figcaption>
-                </figure>
-              }
-              dataSource={['2 Players', '3 Games', 'Consume 1 life']}
-              renderItem={(str: string) => (
-                <List.Item>
-                  <p style={styles.listText}>{str}</p>
-                </List.Item>
-              )}
-            />
+          <Col xs={24} sm={8}>
+            <figure style={styles.iconImg}>
+              <img
+                style={styles.icon}
+                height={lengthMap.modeIcon}
+                alt={modes.practice}
+                src={practiceIcon}
+              />
+              <figcaption style={styles.caption}>{modes.practice}</figcaption>
+            </figure>
           </Col>
-          <Col span={12}>
-            <List
-              size="large"
-              bordered
-              header={
-                <figure style={styles.icon}>
-                  <img
-                    style={styles.icon}
-                    height={fontSizeMap.lg}
-                    alt={practice}
-                    src={practiceIcon}
-                  />
-                  <figcaption style={styles.caption}>{practice}</figcaption>
-                </figure>
-              }
-              dataSource={['1 Player', '1 Game', 'No life needed']}
-              renderItem={(str: string) => (
-                <List.Item>
-                  <p style={styles.listText}>{str}</p>
-                </List.Item>
-              )}
-            />
+          <Col xs={24} sm={8}>
+            <figure style={styles.iconImg}>
+              <img
+                style={styles.icon}
+                height={lengthMap.modeIcon}
+                alt={modes.competition}
+                src={competitionIcon}
+              />
+              <figcaption style={styles.caption}>
+                {modes.competition}
+              </figcaption>
+            </figure>
           </Col>
         </Row>
       </section>
@@ -94,35 +77,123 @@ export const Features = pure(({ textMap }: FeaturesProps) => {
         <p>{section2.desc}</p>
         <Row
           style={styles.imgContainer}
-          gutter={16}
+          gutter={8}
           type="flex"
-          justify="space-around"
+          justify="center"
           align="middle"
         >
           <Col xs={24} sm={8}>
-            <figure style={styles.img}>
+            <figure style={styles.iconImg}>
               <img
                 height={lengthMap.categoryIcon}
-                alt={color}
+                alt={categories.color}
                 src={colorIcon}
               />
-              <figcaption style={styles.caption}>{color}</figcaption>
+              <figcaption style={styles.caption}>{categories.color}</figcaption>
             </figure>
           </Col>
           <Col xs={24} sm={8}>
-            <figure style={styles.img}>
-              <img height={lengthMap.categoryIcon} alt={font} src={fontIcon} />
-              <figcaption style={styles.caption}>{font}</figcaption>
-            </figure>
-          </Col>
-          <Col xs={24} sm={8}>
-            <figure style={styles.img}>
+            <figure style={styles.iconImg}>
               <img
                 height={lengthMap.categoryIcon}
-                alt={layout}
+                alt={categories.font}
+                src={fontIcon}
+              />
+              <figcaption style={styles.caption}>{categories.font}</figcaption>
+            </figure>
+          </Col>
+          <Col xs={24} sm={8}>
+            <figure style={styles.iconImg}>
+              <img
+                height={lengthMap.categoryIcon}
+                alt={categories.layout}
                 src={layoutIcon}
               />
-              <figcaption style={styles.caption}>{layout}</figcaption>
+              <figcaption style={styles.caption}>
+                {categories.layout}
+              </figcaption>
+            </figure>
+          </Col>
+        </Row>
+      </section>
+      <section style={styles.section}>
+        <Heading text={section3.title} />
+        <p>{section3.desc}</p>
+        <Row
+          style={styles.imgContainer}
+          gutter={8}
+          type="flex"
+          justify="center"
+          align="middle"
+        >
+          <Col xs={24} sm={8} lg={4}>
+            <figure>
+              <img
+                style={styles.gameImg}
+                height={lengthMap.categoryIcon}
+                alt={games.brightness}
+                src={brightnessIcon}
+              />
+              <figcaption style={styles.caption}>{games.brightness}</figcaption>
+            </figure>
+          </Col>
+          <Col xs={24} sm={8} lg={4}>
+            <figure>
+              <img
+                style={styles.gameImg}
+                height={lengthMap.categoryIcon}
+                alt={games.colorCode1}
+                src={colorCode1Icon}
+              />
+              <figcaption style={styles.caption}>{games.colorCode1}</figcaption>
+            </figure>
+          </Col>
+          <Col xs={24} sm={8} lg={4}>
+            <figure>
+              <img
+                style={styles.gameImg}
+                height={lengthMap.categoryIcon}
+                alt={games.majorityCase}
+                src={majorityCaseIcon}
+              />
+              <figcaption style={styles.caption}>
+                {games.majorityCase}
+              </figcaption>
+            </figure>
+          </Col>
+          <Col xs={24} sm={8} lg={4}>
+            <figure>
+              <img
+                style={styles.gameImg}
+                height={lengthMap.categoryIcon}
+                alt={games.majorityShape}
+                src={majorityShapeIcon}
+              />
+              <figcaption style={styles.caption}>
+                {games.majorityShape}
+              </figcaption>
+            </figure>
+          </Col>
+          <Col xs={24} sm={8} lg={4}>
+            <figure>
+              <img
+                style={styles.gameImg}
+                height={lengthMap.categoryIcon}
+                alt={games.rectangle}
+                src={rectangleIcon}
+              />
+              <figcaption style={styles.caption}>{games.rectangle}</figcaption>
+            </figure>
+          </Col>
+          <Col xs={24} sm={8} lg={4}>
+            <figure>
+              <img
+                style={styles.gameImg}
+                height={lengthMap.categoryIcon}
+                alt={games.typeface}
+                src={typefaceIcon}
+              />
+              <figcaption style={styles.caption}>{games.typeface}</figcaption>
             </figure>
           </Col>
         </Row>
@@ -133,9 +204,10 @@ export const Features = pure(({ textMap }: FeaturesProps) => {
 
 type StyleKey =
   | 'caption'
+  | 'gameImg'
   | 'icon'
   | 'imgContainer'
-  | 'img'
+  | 'iconImg'
   | 'listText'
   | 'row'
   | 'section';
@@ -148,17 +220,30 @@ const styles: Styles<StyleKey> = {
     textAlign: 'center',
   },
   icon: {
-    margin: spaceMap.sm,
+    marginTop: spaceMap.lg,
+    marginBottom: spaceMap.md,
   },
   imgContainer: {
     marginTop: spaceMap.xl,
   },
-  img: {
+  iconImg: {
+    alignItems: 'center',
     border: `${lengthMap.border} solid ${colorMap.ash}`,
     borderRadius: lengthMap.borderRadius,
     height: lengthMap.categoryImg,
-    paddingTop: spaceMap.lg,
-    marginTop: spaceMap.sm,
+    marginTop: spaceMap.xs,
+    marginRight: 'auto',
+    marginBottom: spaceMap.sm,
+    marginLeft: 'auto',
+    paddingTop: spaceMap.md,
+    width: lengthMap.categoryImg,
+  },
+  gameImg: {
+    alignItems: 'center',
+    border: `${lengthMap.border} solid ${colorMap.ash}`,
+    borderRadius: lengthMap.borderRadius,
+    height: lengthMap.categoryImg,
+    marginTop: spaceMap.xs,
     marginRight: 'auto',
     marginBottom: spaceMap.sm,
     marginLeft: 'auto',
